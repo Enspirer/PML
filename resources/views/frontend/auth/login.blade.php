fb08d5c44a3d4e05f95425c1042292af48a5caf6
Zajjith Vedha <zajjith@gmail.com>
2021-10-15 17:55:02 +0530

    homepage and login page

--- resources/views/frontend/auth/login.blade.php
+++ resources/views/frontend/auth/login.blade.php
@@ -2,96 +2,103 @@
 
 @section('title', app_name() . ' | ' . __('labels.frontend.auth.login_box_title'))
 
+@push('after-styles')
+    <link href="{{ url('css/login.css') }}" rel="stylesheet">
+@endpush
+
 @section('content')
-    <div class="row justify-content-center align-items-center">
-        <div class="col col-sm-8 align-self-center">
-            <div class="card">
-                <div class="card-header">
-                    <strong>
-                        @lang('labels.frontend.auth.login_box_title')
-                    </strong>
-                </div><!--card-header-->
-
-                <div class="card-body">
-                    {{ html()->form('POST', route('frontend.auth.login.post'))->open() }}
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group">
-                                    {{ html()->label(__('validation.attributes.frontend.email'))->for('email') }}
-
-                                    {{ html()->email('email')
-                                        ->class('form-control')
-                                        ->placeholder(__('validation.attributes.frontend.email'))
-                                        ->attribute('maxlength', 191)
-                                        ->required() }}
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
-
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group">
-                                    {{ html()->label(__('validation.attributes.frontend.password'))->for('password') }}
-
-                                    {{ html()->password('password')
-                                        ->class('form-control')
-                                        ->placeholder(__('validation.attributes.frontend.password'))
-                                        ->required() }}
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
-
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group">
-                                    <div class="checkbox">
-                                        {{ html()->label(html()->checkbox('remember', true, 1) . ' ' . __('labels.frontend.auth.remember_me'))->for('remember') }}
-                                    </div>
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
-
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group clearfix">
-                                    {{ form_submit(__('labels.frontend.auth.login_button')) }}
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
-
-                        @if(config('access.captcha.login'))
-                            <div class="row">
-                                <div class="col">
-                                    @captcha
-                                    {{ html()->hidden('captcha_status', 'true') }}
-                                </div><!--col-->
-                            </div><!--row-->
-                        @endif
-
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group text-right">
-                                    <a href="{{ route('frontend.auth.password.reset') }}">@lang('labels.frontend.passwords.forgot_password')</a>
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
-                    {{ html()->form()->close() }}
-
+    <div class="container-fluid p-0 banner login">
+        <div class="container">
+            <div class="row justify-content-center" style="padding-top: 11rem;">
+                <div class="col-10">
                     <div class="row">
-                        <div class="col">
-                            <div class="text-center">
-                                @include('frontend.auth.includes.socialite')
+                        <div class="col-6">
+                            <h3 class="text-white text-center mb-3">Login to your account.</h3>
+                            <form>
+                                <div class="mb-3">
+                                    <label for="email" class="form-label text-white mb-0 ml-3 form-label">Email Address</label>
+                                    <input type="text" class="form-control text-white" value="" name="email" id="email">
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="password" class="form-label text-white mb-0 ml-3 form-label">Password</label>
+                                    <input type="password" class="form-control text-white" id="password" name="password" aria-describedby="password">
+                                </div>
+
+                                <div class="row mb-4">
+                                    <div class="col-6">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="radio" id="radio">
+                                            <label class="form-check-label text-white" for="flexRadioDefault1">Remember Me</label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row mb-4 align-items-center">
+                                    <div class="col-6">
+                                        <a href="#" class="text-decoration-none text-white">Forgot Your Password?</a>
+                                    </div>
+
+                                    <div class="col-6 text-end">
+                                        <button type="submit" class="btn login-btn w-50">Login</button>
+                                    </div>
+                                </div>
+                            </form>
+
+                            <!-- <div class="row align-items-center mt-3">
+                                <div class="col-5 pr-0">
+                                    <hr class="m-0">
+                                </div>
+                                <div class="col-2 text-center">
+                                    <p class="text-white mb-0">or</p>
+                                </div>
+                                <div class="col-5 pl-0">
+                                    <hr class="m-0">
+                                </div>
+                            </div> -->
+
+                            <div class="row justify-content-center mt-3">
+                                <div class="col-12">
+                                    <p class="text-white mb-1" style="font-size: 1rem;">No account? <a href="#" class="text-decoration-none" style="color: #FF7E00; font-size: 1.2rem;">Create one here.</a></p>
+                                    
+                                </div>
+                                
                             </div>
-                        </div><!--col-->
-                    </div><!--row-->
-                </div><!--card body-->
-            </div><!--card-->
-        </div><!-- col-md-8 -->
-    </div><!-- row -->
+                        </div>
+
+                        
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
 @endsection
 
 @push('after-scripts')
     @if(config('access.captcha.login'))
         @captchaScripts
     @endif
+
+
+    <script>
+        $('#email').focus(function() {
+            $(this).siblings('label').addClass('click-input');
+        });
+        $('#email').focusout(function() {
+            if($(this).val() == '' ) {
+                $(this).siblings('label').removeClass('click-input');
+            }
+        });
+
+        $('#password').focus(function() {
+            $(this).siblings('label').addClass('click-input');
+        });
+        $('#password').focusout(function() {
+            if($(this).val() == '' ) {
+                $(this).siblings('label').removeClass('click-input');
+            }
+        });
+    </script>
 @endpush
---
+++ resources/views/frontend/includes/footer.blade.php
@@ -0,0 +1,38 @@
+<section class="container-fluid pt-5 pb-3 text-white" style="background-color: #16202C; margin-top: 5rem;">
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <a href=""><img src="{{ url('img/frontend/logo.png') }}" alt="" class="logo"></a>
+                <p class="text-white mt-3 mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                <p class="text-white mb-3"><i class="fas fa-phone-alt text-white me-3"></i></i>+94 777000000</p>
+
+                <p class="text-white mb-3"><i class="fas fa-envelope text-white me-3"></i>info@propertymarketlive.com</p>
+
+                <p class="text-white mb-3"><i class="fas fa-map-marker-alt text-white me-3"></i>123/A, Colombo 05, Sri Lanka.</p>
+            </div>
+            <div class="col-3 ps-5">
+                <h5 class="fw-bolder mt-2 mb-4">Top Products</h5>
+                <a href="" class="mb-3 d-block text-decoration-none no-result-list text-white">Tips for buyers</a>
+                <a href="" class="mb-3 d-block text-decoration-none no-result-list text-white">Tips for sellers</a>
+                <a href="" class="mb-3 d-block text-decoration-none no-result-list text-white">Commercial Resources</a>
+                <a href="" class="mb-3 d-block text-decoration-none no-result-list text-white">Marketing Service</a>
+            </div>
+            <div class="col-3 ps-5">
+                <h5 class="text-white fw-bolder mt-2 mb-4">Newsletter</h5>
+                <p class="text-white">You can trust us. we only send promo offers, not a single spam.</p>
+                <form>
+                    {{csrf_field()}}
+                    <div class="input-group mt-4 mb-3 p-1" style="background-color: rgb(255, 254, 252, 0.1); border: 1px solid white; border-radius: 50px">
+                        <input type="text" class="form-control border-0" placeholder="Type your Email" aria-label="email" aria-describedby="email" name="email" style="background: border-box; border-radius: 50px; color: white">
+                        <button class="btn rounded-0 text-light border-0" type="submit" style="background-color: #F60331; border-radius: 50px!important; font-size: 0.6rem;">SUBSCRIBE</button>
+                    </div>
+                </form>
+            </div>
+            <div class="col-3 ps-5">
+                <h5 class="fw-bolder mt-2 mb-4">About Us</h5>
+                <p class="text-white">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+        </div>
+    </div>
+</section>
