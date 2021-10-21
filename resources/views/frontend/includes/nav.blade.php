f13f0d64a3b3ccba969fcb88d367239c557803df
Zajjith Vedha <zajjith@gmail.com>
2021-10-21 18:10:48 +0530

    user account information

--- resources/views/frontend/includes/nav.blade.php
+++ resources/views/frontend/includes/nav.blade.php
@@ -39,7 +39,7 @@
                     <img src="{{ url('img/frontend/user.png') }}" class="rounded-circle" style="height: 50px; width: 50px; margin-right: 1.4rem;"> <span class="text-white fw-bold user-name">{{auth()->user()->first_name}}</span>
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                  <a class="dropdown-item" href="">My Account</a>
+                  <a class="dropdown-item" href="{{ route('frontend.user.dashboard') }}">My Account</a>
                   <a class="dropdown-item" href="">My Settings</a>
                   <!-- <a class="dropdown-item" href="">My Notification Settings</a> -->
                   <a class="dropdown-item" href="{{ route('frontend.auth.logout') }}" style="border-bottom: none">Log Out</a>
