0758c3cdb210766bc032cddd1f9a7e99c0ee3580
sanjayaharshana <sanjayaharasana@gmail.com>
2021-12-29 16:49:22 +0530

    Lat Longers

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -360,8 +360,8 @@ class HomeController extends Controller
                 $impack_array = [
                     'I' => $cold_data->id,
                     'T' => 2,
-                    'X' => $cold_data->lat,
-                    'Y' => $cold_data->long,
+                    'X' => $cold_data->long,
+                    'Y' => $cold_data->lat,
                     'C' => 2
                 ];
                 array_push($outArray,$impack_array);
