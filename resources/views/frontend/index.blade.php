de4bd66acd81a0d740c975fb596ae9bee3c7c174
prasadbanuka <banuka.mawelage@gmail.com>
2021-12-31 16:45:25 +0530

    map center change

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -40,7 +40,7 @@
         mapDiv = document.getElementById('map');
 
         var map = new google.maps.Map(mapDiv, {
-            center: new google.maps.LatLng(53.3427371467871, -6.25911712396487),
+            center: new google.maps.LatLng(6.9271, 79.8612),
             zoom: 12,
             mapTypeId: google.maps.MapTypeId.ROADMAP,
             styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}] // "Subtle Grayscale" style found on snazzymaps.com
