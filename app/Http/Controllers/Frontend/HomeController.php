1ee09a99ca1f0faa59c0e851378818d3ee0ce370
sanjayaharshana <sanjayaharasana@gmail.com>
2022-01-03 17:48:18 +0530

    Home Panel Contoller

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -564,14 +564,15 @@ class HomeController extends Controller
         foreach ($properties as $property)
         {
            $distance =   self::haversineGreatCircleDistance($lat,$lng,$property->lat,$property->long);
+            $distandPanel =  round($distance/1000,2);
+           if($distandPanel > 10 ){
 
-          $distandPanel =  round($distance/1000,2);
-
-          $stakGroup = [
-              'propery_id' => $property->id,
-              'distance' => $distandPanel. ' KM',
-              'property' => $property->name
-          ];
+               $stakGroup = [
+                   'propery_id' => $property->id,
+                   'distance' => $distandPanel. ' KM',
+                   'property' => $property->name
+               ];
+           }
           array_push($finalOut,$stakGroup);
 
         }
