15d4bebfad2c5aff3b6446ac1a2b72025266b1bd
prasadbanuka <banuka.mawelage@gmail.com>
2022-01-19 16:25:17 +0530

    mobile nav

--- resources/views/frontend/includes/nav.blade.php
+++ resources/views/frontend/includes/nav.blade.php
@@ -230,7 +230,7 @@
             <form action="{{route('frontend.country_change')}}" method="post" style="margin-bottom:25px;">
                 {{csrf_field()}}
 
-                <select name="countries_list" class="position-absolute me-3" onchange="this.form.submit()">
+                <select name="countries_list" class="position-absolute me-3 mobile-country-list" onchange="this.form.submit()">
                     <option value="" selected disabled>Select Your Country</option>
 
                     @foreach($tpr_countries as $tpr_country)
