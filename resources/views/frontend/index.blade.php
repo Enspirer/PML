d5137f6e65f16268b62b3407ceb8c9cffdeefada
nihsaan7 <nihsaan.enspirer@gmail.com>
2022-02-10 08:30:13 +0530

    index issue fixed

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -610,7 +610,7 @@
                     <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpropertymarketlive&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="100%" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
 
                 </div>               
-                                
+                    @if($featured_property_news != null)          
                         <div class="col-4 col-xs-12 col-tab-50">
                             <a href="{{url('news')}}" style="color:black; text-decoration:none"></a>
                                 <div class="card position-relative" style="height: 28.2rem;"><a href="{{url('news')}}" style="color:black; text-decoration:none">
